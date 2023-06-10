88d5f92454ab1f8ecf37c251f907bfb9a2e43b09
Mattia Caprioli <capriolimattia1994@gmail.com>
2023-06-10 18:17:58 +0700

    creazione progetto

---
+++ src/components/ImageGallary.vue
@@ -0,0 +1,53 @@
+<script setup>
+  import { defineProps } from "vue"
+
+  const p = defineProps(["isWithColor"]);
+
+  const copyUrl = async (url) => {
+    await navigator.clipboard.writeText(url)
+  }
+</script>
+
+<template>
+    <VCard class="mx-5 my-2 pa-3">
+        <VRow>
+            <vCol
+            v-for="n in 200"
+            cols="4"
+            sm="3"
+            md="2"
+            lg="1"
+            >
+                <VHover
+                    v-slot="{isHovering, props}"
+                >
+                    <vCard
+                    :elevation="isHovering ? 12 : 2"
+                    v-bind="props"
+                    @click="copyUrl(`https://picsum.photos/500/300?image=${n * 5 + 10}${p.isWithColor ? '' : '&grayscale'}`)"
+                    >
+                        <VImg
+                        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}${p.isWithColor ? '' : '&grayscale'}`"
+                        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}${p.isWithColor ? '' : '&grayscale'}`"
+                        aspect-ratio="1"
+                        cover
+                        >
+                            <template v-slot:placeholder>
+                                <VRow
+                                class="fill-height ma-0"
+                                align="center"
+                                justify="center"
+                                >
+                                    <VProgressCircular
+                                        indeterminate
+                                        color="grey-lighten-5"
+                                    ></VProgressCircular>
+                                </VRow>
+                            </template>
+                        </VImg>
+                    </vCard>
+                </VHover>
+            </vCol>
+        </VRow>
+    </VCard>
+</template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import { defineEmits } from 'vue'
+
+  const emits = defineEmits(['selectOption'])
+
+  const selectOption = (isColor) => {
+    emits("selectOption", isColor)
+  }
+</script>
+
+<template>
+    <VNavigationDrawer
+      expand-on-hover
+      rail
+    >
+    <VList>
+        <VListItem
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Lily Dhata"
+            subtitle="lily@gmailcom"
+            @click="selectOption(true)"
+        ></VListItem>
+    </VList>
+
+    <VDivider/>
+
+    <VList density="compact" nav>
+        <VListItem
+          prepend-icon="mdi-image"
+          title="Color Gallery"
+          value="color"
+        />
+        <VListItem
+          prepend-icon="mdi-dialpad"
+          title="Grayspace Gallery"
+          value="color"
+          @click="selectOption(false)"
+        />
+
+
+    </VList>
+    </VNavigationDrawer>
+</template>
